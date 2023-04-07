dee711b62dc62c342bd7aba8ee84b565b7f2e178
Matt McLarty <admin@miniversal.org>
2023-04-07 14:11:19 -0400

    Create wp-header-footer-loader.php

---
+++ wp-header-footer-loader.php
@@ -0,0 +1,22 @@
+/**
+ * Plugin Name
+ *
+ * @package           miniversal-wp
+ * @author            Miniversal
+ * @copyright         2023 Miniversal
+ * @license           GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       wp-header-footer-loader
+ * Plugin URI:        https://miniversal.org/wp-plugins/wp-header-footer-loader
+ * Description:       Plugin that provides a URL to load the Header or Footer template of your Wordpress site from an external site/app
+ * Version:           1.0.0
+ * Requires at least: 6.1
+ * Requires PHP:      8.3
+ * Author:            Miniversal
+ * Author URI:        https://miniversal.org
+ * Text Domain:       plugin-slug
+ * License:           GPL v3 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Update URI:        https://miniversal.org/wp-plugins/wp-header-footer-loader/update
+ */
